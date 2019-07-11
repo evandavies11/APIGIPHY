ed588ce29c12bf21513df26cd94f4fc4a6983daf
evandavies11 <evandavies11@gmail.com>
2019-07-11 15:36:22 -0500

    technically whats needed

--- javascript/app.js
+++ javascript/app.js
@@ -1,21 +1,66 @@
 console.log("hello world")
 
-var api = "https://api.giphy.com/v1/gifs/search?";
-var apiKey = "&api_key=CRFy3PHJVIfBdRWsSbt92lLPiA4SoQuF";
-var query = "&q=rainbow";
+$("button").on("click", function () {
+    // In this case, the "this" keyword refers to the button that was clicked
+    var person = $(this).attr("data-person");
 
-function setup() {
-    noCanvas();
-    var url = api + apiKey + query;
-    loadJSON(url, gotData);
-}
+    // Constructing a URL to search Giphy for the name of the person who said the quote
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        person + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
 
-function gotData(giphy) {
-    for (var i = 0; i < giphy.data.length; i++) {
-        createImg(giphy.data[i].images.original.url);
+    // Performing our AJAX GET request
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        // After the data comes back from the API
+        .then(function (response) {
+            // Storing an array of results in the results variable
+            var results = response.data;
+
+            // Looping over every result item
+            for (var i = 0; i < results.length; i++) {
+
+                // Only taking action if the photo has an appropriate rating
+                if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+                    // Creating a div for the gif
+                    var gifDiv = $("<div>");
+
+                    // Storing the result item's rating
+                    var rating = results[i].rating;
+
+                    // Creating a paragraph tag with the result item's rating
+                    var p = $("<p>").text("Rating: " + rating);
+
+                    // Creating an image tag
+                    var personImage = $("<img>");
+
+                    // Giving the image tag an src attribute of a proprty pulled off the
+                    // result item
+                    personImage.attr("src", results[i].images.fixed_height.url);
+
+                    // Appending the paragraph and personImage we created to the "gifDiv" div we created
+                    gifDiv.append(p);
+                    gifDiv.append(personImage);
+
+                    // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+                    $("#gifs-appear-here").prepend(gifDiv);
+                }
+            }
+        });
+});
+
+$(".gif").on("click", function () {
+    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+    var state = $(this).attr("data-state");
+    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+    // Then, set the image's data-state to animate
+    // Else set src to the data-still value
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
     }
-}
-
-function draw() {
-
-}
+});
